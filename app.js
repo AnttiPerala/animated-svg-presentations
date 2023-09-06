0076143ade4260389ef032627ec36f9b51d71c55
AnttiPerala <99724071+AnttiPerala@users.noreply.github.com>
2023-09-06 23:44:08 +0300

    deleting works

--- app.js
+++ app.js
@@ -14,14 +14,49 @@ window.onload = function() {
     let animatedPath;
     let offset = 0;
 
-    // Functions for Selection
+// Set initial canvas dimensions
+paper.view.viewSize = new paper.Size(document.getElementById('myCanvas').clientWidth, document.getElementById('myCanvas').clientHeight);
+
+// Adjust canvas dimensions on window resize
+window.addEventListener('resize', function() {
+    paper.view.viewSize = new paper.Size(document.getElementById('myCanvas').clientWidth, document.getElementById('myCanvas').clientHeight);
+});
+    
+    paper.view.viewSize = new paper.Size(window.innerWidth, window.innerHeight);
+
+
     window.toggleSelectMode = function() {
         selectMode = !selectMode;
-        if (selectedPath) {
+        if (!selectMode && selectedPath) {
+            clearBoundingBox(); // Clear the bounding box when exiting select mode
             selectedPath.selected = false;
             selectedPath = null;
         }
     }
+    
+ 
+
+    let boundingBox = null; // Variable to hold the bounding box
+
+function showBoundingBox(targetPath) {
+    if (targetPath) {
+        clearBoundingBox(); // Clear any existing bounding box
+
+        // Create a rectangle from the bounds of the selected path
+        boundingBox = new paper.Path.Rectangle(targetPath.bounds);
+        boundingBox.strokeColor = 'blue';
+        boundingBox.strokeWidth = 2;
+        boundingBox.dashArray = [10, 4];  // Dashed line style for the bounding box
+    }
+}
+
+function clearBoundingBox() {
+    if (boundingBox) {
+        boundingBox.remove();
+        boundingBox = null;
+    }
+}
+
 
     // Functions for Editing
     window.toggleEditMode = function() {
@@ -65,11 +100,15 @@ window.onload = function() {
         if (pathsToAnimate.length === 0) return;
         offset = 0;
         let currentPath = pathsToAnimate.shift();
+        
+        // Ensure the current path is not selected
+        currentPath.selected = false;
+    
         currentPath.strokeColor = 'transparent';
         animatedPath = new paper.Path();
         animatedPath.strokeColor = 'black';
         animatedPath.strokeWidth = currentPath.strokeWidth;
-
+    
         paper.view.onFrame = function(event) {
             offset += 2;
             if (offset > currentPath.length) {
@@ -81,6 +120,7 @@ window.onload = function() {
             animatedPath.add(point);
         };
     }
+    
 
     // Functions for Path Simplification
     window.simplifyPath = function() {
@@ -95,6 +135,17 @@ window.onload = function() {
 
     // Mouse events
     tool.onMouseDown = function(event) {
+        if (editMode && selectedPath) {
+            let hitResult = selectedPath.hitTest(event.point, {
+                segments: true,
+                tolerance: 5
+            });
+            if (hitResult && hitResult.type === 'segment') {
+                segment = hitResult.segment;  // Store the segment for use in onMouseDrag
+                return;  // Exit the function here to prevent further logic execution
+            }
+        }
+    
         if (selectMode) {
             let hitResult = paper.project.hitTest(event.point, {
                 fill: true,
@@ -103,18 +154,11 @@ window.onload = function() {
             });
             if (hitResult && hitResult.item instanceof paper.Path) {
                 if (selectedPath) {
+                    clearBoundingBox();
                     selectedPath.selected = false;
                 }
                 selectedPath = hitResult.item;
-                selectedPath.selected = true;
-            }
-        } else if (editMode && selectedPath) {
-            let hitResult = selectedPath.hitTest(event.point, {
-                segments: true,
-                tolerance: 5
-            });
-            if (hitResult && hitResult.type === 'segment') {
-                segment = hitResult.segment;
+                showBoundingBox(selectedPath); 
             }
         } else {
             path = new paper.Path({
@@ -123,10 +167,14 @@ window.onload = function() {
             });
         }
     }
+    
+    
 
     tool.onMouseDrag = function(event) {
         if (editMode && segment) {
-            segment.point = segment.point.add(event.delta);
+            segment.point = segment.point.add(event.delta);  // Move the segment
+            
+            // Update the corresponding segment indicator's position
             let index = selectedPath.segments.indexOf(segment);
             if (index !== -1 && segmentIndicators[index]) {
                 segmentIndicators[index].position = segment.point;
@@ -135,10 +183,13 @@ window.onload = function() {
             path.add(event.point);
         }
     }
+    
+    
 
     tool.onMouseUp = function(event) {
-        segment = null;
+        segment = null;  // Clear the segment reference on mouse up
     }
+    
 
     // Function to Save as SVG
     window.saveSVG = function() {
@@ -152,4 +203,24 @@ window.onload = function() {
             a.click();
         }
     }
-}
+
+    window.deleteSelectedPath = function() {
+        if (selectedPath) {
+            // Remove segment indicators if in edit mode
+            if (editMode) {
+                clearSegmentIndicators();
+            }
+            
+            // Remove the selected path from the canvas
+            selectedPath.remove();
+            
+            // Clear the bounding box
+            clearBoundingBox();
+            
+            // Reset the selected path reference
+            selectedPath = null;
+        }
+    }
+    
+    
+}/* End window onload */
